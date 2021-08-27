1a62929dd53940363712878d4ff89391bb4dfcc7
habeshawit <shalomseleshi@gmail.com>
2021-08-26 21:59:41 -0700

    make dice visible

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const score0El = document.getElementById('score--0')
+const score1El = document.getElementById('score--1')
+const diceEl = document.querySelector('.dice')
+const rollDice = document.querySelector('.btn--roll')
+const newRoll = document.querySelector('.btn--new')
+const hold = document.querySelector('.btn--hold')
+
+//initial status
+score0El.textContent = 0
+score1El.textContent = 0
+diceEl.classList.add('hidden')
+
+rollDice.addEventListener('click', function(){
+    diceEl.classList.remove('hidden')
+})
